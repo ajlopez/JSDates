3efa2715718c0c197699a0325ce4e5d0099d4e5f
ajlopez <webmaster@ajlopez.com>
2016-01-16 11:51:47 -0300

    today to date

--- test/dates.js
+++ test/dates.js
@@ -9,13 +9,28 @@ exports['create today'] = function (test) {
     
     var date = new Date();
     
-    test.equal(today.year(), date.getYear());
+    test.equal(today.year(), date.getFullYear());
     test.equal(today.month(), date.getMonth() + 1);
     test.equal(today.day(), date.getDate());
-    test.equal(today.hour(), 0);
-    test.equal(today.minute(), 0);
-    test.equal(today.second(), 0);
+    test.equal(today.hours(), 0);
+    test.equal(today.minutes(), 0);
+    test.equal(today.seconds(), 0);
 };
 
-
+exports['today to date'] = function (test) {
+    var today = jsdates.today().toDate();
+    
+    test.ok(today);
+    test.equal(typeof today, 'object');
+    test.ok(today instanceof Date);
+    
+    var date = new Date();
+    
+    test.equal(today.getFullYear(), date.getFullYear());
+    test.equal(today.getMonth(), date.getMonth());
+    test.equal(today.getDate(), date.getDate());
+    test.equal(today.getHours(), 0);
+    test.equal(today.getMinutes(), 0);
+    test.equal(today.getSeconds(), 0);
+};
 
