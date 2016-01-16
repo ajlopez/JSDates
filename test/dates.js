8b4f0ea07ec835dd2cabab1046bb6cfb6daafb11
ajlopez <webmaster@ajlopez.com>
2016-01-16 11:57:45 -0300

    now test

--- test/dates.js
+++ test/dates.js
@@ -34,3 +34,18 @@ exports['today to date'] = function (test) {
     test.equal(today.getSeconds(), 0);
 };
 
+exports['create now'] = function (test) {
+    var now = jsdates.now();
+    
+    test.ok(now);
+    test.equal(typeof now, 'object');
+    
+    var date = new Date();
+    
+    test.equal(now.year(), date.getFullYear());
+    test.equal(now.month(), date.getMonth() + 1);
+    test.equal(now.day(), date.getDate());
+    test.equal(now.hours(), date.getHours());
+    test.equal(now.minutes(), date.getMinutes());
+    test.equal(now.seconds(), date.getSeconds());
+};
