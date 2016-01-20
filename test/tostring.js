719e31412e72d8f3b3d25a29c60fb396cb88150a
ajlopez <webmaster@ajlopez.com>
2016-01-20 06:42:42 -0300

    datetime to string with simple format

--- test/tostring.js
+++ test/tostring.js
@@ -31,3 +31,14 @@ exports['datetime to string'] = function (test) {
     test.equal(typeof result, 'string');
     test.equal(result, "20160118 220450");
 };
+
+exports['datetime to string with simple format'] = function (test) {
+    var date = jsdates.datetime(2016,1,18, 22, 4, 50);
+    
+    var result = date.toString({ format: true });
+    
+    test.ok(result);
+    test.equal(typeof result, 'string');
+    test.equal(result, "2016-01-18 22:04:50");
+};
+
